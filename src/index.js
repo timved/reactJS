531521a0df6fcd0d187e02160f9d78510826aa14
timofey <timved@mail.ru>
2018-06-14 19:08:14 +0300

    + испарвлены замечания 2 дз

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import Developer from './developer';
 
-const developer = new Developer('Jon','Bon');
-let nameDev = document.getElementById("developer");
-let date = new Date();
+const developer = new Developer('Jon' , 'Bon');
+const nameDev = document.getElementById('developer');
+const date = new Date();
 
 nameDev.innerHTML = developer.userFirstName() + ' ' + developer.userLastName();
 nameDev.onclick = function() {
-    document.getElementById("date").innerHTML = date.getDate() +'/'+ (date.getMonth()+1) +'/'+ date.getFullYear();
+    document.getElementById('date').innerHTML = date.getDate() +'/'+ (date.getMonth()+1) +'/'+ date.getFullYear();
 }
